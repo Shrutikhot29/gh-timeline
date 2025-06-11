f811acaaa52d6e949cdb06ecc22fe0567f9c5787
Shrutikhot29 <shruti.khot2906@gmail.com>
2025-06-11 17:45:12 +0530

    Reset main branch to empty

--- src/setup_cron.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CRON_CMD="*/5 * * * * /usr/bin/php $(pwd)/cron.php"
-( crontab -l 2>/dev/null | grep -v -F "$CRON_CMD"; echo "$CRON_CMD" ) | crontab -
-echo "CRON job installed to run every 5 minutes."
